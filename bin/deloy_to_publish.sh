af0522ea7e1a9c1d8e4437aa07b9f4dc382e9673
baizhe <919041098@qq.com>
2022-10-14 23:20:42 +0800

    feat:配置打包base，创建打包脚本

---
+++ bin/deloy_to_publish.sh
@@ -0,0 +1,10 @@
+#!usr/bin/env bash
+rm -rf dist
+npm run build
+cd dist
+git init
+git add .
+git commit -m deloy
+git remote add origin https://github.com/Lu9709/mangosteen-font-react.git
+git push -f origin master:master
+cd -
