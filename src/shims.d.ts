3c76d5181c4effbe4d0a0557b2f401ff3786b07b
baizhe <919041098@qq.com>
2022-10-31 17:09:38 +0800

    chore(shims.d.ts):修改

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,12 @@
-import type { AttributifyAttributes } from "unocss/preset-attributify";
-
-declare module "react" {
-  interface HTMLAttributes<T> extends AttributifyAttributes {
-    flex?: boolean;
+import * as React from 'react'
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    flex?: boolean
+    relative?: boolean
+    text?: string
+    grid?: boolean
+    before?: string
+    after?: string
+    shadow?: boolean
   }
 }
