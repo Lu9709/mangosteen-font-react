130470ee72576bc20e298424c26e02640c4223c0
baizhe <919041098@qq.com>
2022-11-03 14:53:42 +0800

    fix: 解决TS报错

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -14,7 +14,7 @@ export const WelcomeLayout: React.FC = () => {
   const location = useLocation() // 获取当前地址栏的信息
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
-  const [extraStyle, setExtraStyle] = useState({ position: 'relative' })
+  const [extraStyle, setExtraStyle] = useState<{ position: 'relative' | 'absolute' }>({ position: 'relative' })
   const transitions = useTransition(location.pathname, {
     from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
