c4118cb18ca1be1f9612052eeef40837e5e62993
baizhe <919041098@qq.com>
2022-11-03 14:41:48 +0800

    feat: 添加一个默认404路由

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { RedirectToWelcome1 } from '../components/RedirectToWelcome1'
 import { MainLayout } from '../layouts/MainLayouts'
+import { NotFoundPage } from '../pages/NotFoundPage'
 import { WelcomeRoutes } from './WelcomeRoutes'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <RedirectToWelcome1 />,
+    errorElement: <NotFoundPage />,
     children: [
       WelcomeRoutes
     ]
