208f38053d4f7731565c5e4c3e0ac9219d095131
baizhe <919041098@qq.com>
2022-11-16 21:26:45 +0800

    fix(mock): 修复 mock bug，添加远程数据类型，http.get 接受泛型，完善 Home 页面展示逻辑

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,24 @@
 import useSWR from 'swr'
+import { Navigate } from 'react-router-dom'
 import p from '../assets/images/pig.svg'
 import add from '../assets/icons/add.svg'
 import { ajax } from '../lib/ajax'
 export const Home: React.FC = () => {
-  const { data: meData, error: meError } = useSWR('/api/v1/me', (path) => {
-    return ajax.get(path)
-  })
-  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, (path) => {
-    return ajax.get(path)
-  })
-  console.log(meData, meError, itemsData, itemsError)
-  // npm run dev 开发 /api/v1/me
-  // npm run build 上线 http://121.196.236.94:8080/api/v1
+  const { data: meData, error: meError } = useSWR('/api/v1/me', async path =>
+    (await ajax.get<Resource<User>>(path)).data.resource
+  )
+  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, async path =>
+    (await ajax.get<Resources<Item>>(path)).data
+  )
+
+  const isLoadingMe = !meData && !meError
+  const isLoadingItems = meData && !itemsData && !itemsError
+
+  if (isLoadingMe || isLoadingItems)
+    return <div>加载中……</div>
+
+  if (itemsData?.resources[0])
+    return <Navigate to="/items" />
 
   return <>
     <div flex justify-center items-center>
