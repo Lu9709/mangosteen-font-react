bb729ecb6fcc5b060c0df3c48526f495855731bf
baizhe <919041098@qq.com>
2022-11-16 21:29:51 +0800

    feat(hooks): 创建useTitle hooks,演示如何设置页面标题 title

---
+++ src/hooks/useTitle.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+
+export const useTitle = (title?: string) => {
+  useEffect(() => {
+    if (title === undefined || title === null)
+      return
+    document.title = title
+  }, [])
+}
