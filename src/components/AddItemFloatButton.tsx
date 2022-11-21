cdd636b1ec4cd670220eaf547a8f5e6152b38039
baizhe <919041098@qq.com>
2022-11-21 18:37:53 +0800

    chore(uno.config.ts): 将 .j-icon 的代码放入 components layer

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,9 +1,9 @@
-import { Icon } from './Icons'
+import { Icon } from './Icon'
 
 export const AddItemFloatButton: React.FC = () => {
   return (
     <button w-56px h-56px bg="#5C33BE" rounded="50%" b-none text-white
-    fixed bottom-16px right-16px flex justify-center items-center>
+      fixed bottom-16px right-16px flex justify-center items-center>
       <Icon name="add" className="w-48px h-48px" />
     </button>
   )
