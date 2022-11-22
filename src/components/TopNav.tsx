1258185f6896922b80c692b78b5aa2ee20915333
baizhe <919041098@qq.com>
2022-11-22 11:11:23 +0800

    feat(components): 完成 Topnav 组件

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,3 +1,13 @@
-export const TopNav: React.FC = () => {
-  return <div>TopNav </div>
+import { Icon } from './Icon'
+
+interface Props {
+  title?: string
+}
+export const TopNav: React.FC<Props> = ({ title = '山竹记账' }) => {
+  return (
+    <div text-white flex items-center p-16px>
+      <Icon name="menu" className="w-24px h-24px mr-16px" />
+      <h1 text-24px>{title}</h1>
+    </div>
+  )
 }
