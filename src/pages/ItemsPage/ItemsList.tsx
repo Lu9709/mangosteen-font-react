92da17393fe93313217b5bff4014f857ce889bb6
baizhe <919041098@qq.com>
2022-12-01 14:43:47 +0800

    feat(pages): 完成加载中和加载失败的展示逻辑

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
 import useSWRInfinite from 'swr/infinite'
 import { ajax } from '../../lib/ajax'
 
 interface Props {}
+
+const Div = styled.div`
+  padding: 16px;
+  text-align: center
+`
 const getKey = (pageIndex: number, prev: Resources<Item>) => {
   if (prev) {
     const sendCount = (prev.pager.page - 1) * prev.pager.per_page + prev.resources.length
@@ -19,8 +25,14 @@ export const ItemsList: React.FC<Props> = () => {
   const onLoadMore = () => {
     setSize(size + 1)
   }
+  const isLoadingInitialData = !data && !error
+  const isLoadingMore = data?.[size - 1] === undefined && !error
+  const isLoading = isLoadingInitialData || isLoadingMore
   if (!data) {
-    return <span>还没搞定</span>
+    return <div>
+      { error && <Div>数据加载失败，请刷新页面</Div> }
+      { isLoading && <Div>数据加载中...</Div>}
+    </div>
   } else {
     const last = data[data.length - 1]
     const { pager: { page, per_page, count } } = last
@@ -48,10 +60,13 @@ export const ItemsList: React.FC<Props> = () => {
           )
         })
       }</ol>
+      { error && <Div>数据加载失败，请刷新页面</Div> }
       {
-        hasMore
-          ? <div p-16px><button j-btn onClick={onLoadMore}>加载更多</button></div>
-          : <div p-16px text-center>没有更多数据了</div>
+        !hasMore
+          ? <Div>没有更多数据了</Div>
+          : isLoading
+            ? <Div>数据加载中...</Div>
+            : <Div><button j-btn onClick={onLoadMore}>加载更多</button></Div>
       }
     </>
   }
