5c5f23bea1340ca15af98d92da18631bf668e9fd
baizhe <919041098@qq.com>
2022-12-05 15:59:46 +0800

    feat(pages): 表单提交事件可以获取到表单数据

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,15 @@
+import type { FormEventHandler } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { useSignInStore } from '../stores/useSignInStore'
 
 export const SignInPage: React.FC = () => {
+  const { data, setData } = useSignInStore()
+  const onsubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
   return (
     <div>
       <Gradient>
@@ -12,15 +19,16 @@ export const SignInPage: React.FC = () => {
         <Icon name="logo" className='w-64px h-68px' />
         <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
       </div>
-      <form j-form>
+      <form j-form onSubmit={onsubmit}>
+        <div b-1 b-red>{ JSON.stringify(data) }</div>
         <div>
           <span j-form-label>邮箱地址</span>
-          <input j-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码' />
+          <input j-input-text type="text" onChange={e => setData({ email: e.currentTarget.value })} placeholder='请输入邮箱，然后点击发送验证码' />
         </div>
         <div>
           <span j-form-label>验证码</span>
           <div flex gap-x-16px>
-            <input j-input-text type="text" placeholder='六位数字'/>
+            <input j-input-text type="text" onChange={e => setData({ code: e.currentTarget.value })} placeholder='六位数字'/>
             <button j-btn>发送验证码</button>
           </div>
         </div>
