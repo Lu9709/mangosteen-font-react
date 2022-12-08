0460ec2dc9098c0d185e001d689296e2ddab67e0
baizhe <919041098@qq.com>
2022-12-08 15:09:51 +0800

    chore(index.html): 修改title标题

--- bin/deloy_to_publish.sh
+++ bin/deloy_to_publish.sh
@@ -5,6 +5,6 @@ cd dist
 git init
 git add .
 git commit -m deloy
-git remote add origin https://github.com/Lu9709/mangosteen-font-react.git
+git remote add origin https://github.com/Lu9709/mangosteen-font-react-web.git
 git push -f origin master:master
 cd -
