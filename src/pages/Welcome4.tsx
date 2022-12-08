b80804b200b79d05ab6463afd75b83d8850765a7
baizhe <919041098@qq.com>
2022-12-08 15:25:22 +0800

    feat(pages): 修改 Welcome 界面，即使页面被浏览器挡住，也不会影响交互

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import p from '../assets/images/welcome4.svg'
+
 export const Welcome4: React.FC = () => {
   return (
     <div text-center>
@@ -7,6 +9,9 @@ export const Welcome4: React.FC = () => {
       云备份 <br />
         再也不怕数据丢失
       </h2>
+      <div mt-64px>
+        <Link text-32px color="#6035BF" font-bold to="/welcome/xxx">开启应用</Link>
+      </div>
     </div>
   )
 }
