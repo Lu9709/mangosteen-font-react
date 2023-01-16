e6d0fc192064912167b910e2cf6c8ca78ac35f1b
baizhe <919041098@qq.com>
2023-01-16 15:57:56 +0800

    perf(componenst): tabs 支持自定义三处的样式

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,19 +1,21 @@
+import type { ReactNode } from 'react'
 import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import s from './ItemsNewPage.module.scss'
 
 type ItemKind = 'income' | 'expenses'
-type TabItem = { key: ItemKind; text: string }[]
+type TabItem = { key: ItemKind; text: string; element?: ReactNode }[]
 export const ItemsNewPage: React.FC = () => {
   const tabItems: TabItem = [{ key: 'expenses', text: '支出' }, { key: 'income', text: '收入' }]
   const [tabItem, useTabItem] = useState<ItemKind>('expenses')
   return (
-    <div>
+    <div className={s.wrapper}>
       <Gradient>
         <TopNav title="记一笔" icon={<Icon name="back" />} />
-        <Tabs tabItems={tabItems} className='children-flex-1 text-center'
+        <Tabs tabItems={tabItems} className="text-center" classPrefix='itemsNewPage'
           value={tabItem}
           onChange={(item) => { useTabItem(item) }}
         />
